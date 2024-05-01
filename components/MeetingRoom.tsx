8685c158fb24fd99e8055b366fe45a55ca201f84
JC-Coder <josephchimezie2003@gmail.com>
2024-05-01 22:36:04 +0100

    update

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -6,6 +6,15 @@ import {
 } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react';
 import { cn } from '../lib/utils';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { LayoutList } from 'lucide-react';
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
@@ -39,8 +48,32 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
 
-        <div className="fixed bottom-0 flex w-full items-center justify-center">
+        <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
           <CallControls />
+
+          <DropdownMenu>
+            <div className="flex items-center">
+              <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+                <LayoutList size={20} className="text-white" />
+              </DropdownMenuTrigger>
+            </div>
+
+            <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+              {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                <div key={index}>
+                  <DropdownMenuItem
+                  className='cursor-pointer hover:bg-[#4c535b]'
+                    onClick={() =>
+                      setLayout(item.toLowerCase() as CallLayoutType)
+                    }
+                  >
+                    {item}
+                  </DropdownMenuItem>
+                </div>
+              ))}
+              <DropdownMenuSeparator />
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </section>
