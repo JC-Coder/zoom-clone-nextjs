9e9706abf5c6bb19d13a39152ca63b64dd0b6ca4
JC-Coder <josephchimezie2003@gmail.com>
2024-05-01 01:22:46 +0100

    add create call feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from '@/components/ui/toaster';
+
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,7 +35,10 @@ export default function RootLayout({
           }
         }}
       >
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
       </ClerkProvider>
     </html>
   );
